59b15f43a10c022162ce1bb24168a569f233d64e
mnabhiro <mnabhiro@qti.qualcomm.com>
2018-07-28 21:38:36 +0530

    Project files

--- src/main/java/com/example/vendor/controller/VendorController.java
+++ src/main/java/com/example/vendor/controller/VendorController.java
@@ -2,6 +2,7 @@ package com.example.vendor.controller;
 
 import com.example.vendor.model.Vendor;
 import com.example.vendor.model.VendorConnection;
+import com.example.vendor.repository.VendorConnectionRepository;
 import com.example.vendor.repository.VendorRepository;
 import com.example.vendor.service.VendorService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/vendor")
@@ -22,15 +23,29 @@ public class VendorController {
     @Autowired
     private VendorRepository vendorRepository;
 
+    @Autowired
+    private VendorConnectionRepository vendorConnectionRepository;
+
     @RequestMapping("/{id}")
     @ResponseBody
-    public VendorConnection getVendorConnection(@PathVariable("id") String vendorId) {
-        Optional<Vendor> vendor = vendorRepository.findById(Long.valueOf(vendorId));
-        if (vendor.isPresent()) {
-            return vendorService.getVendorConnection(vendor.get());
-        } else {
-            return null;
-        }
+    public Map<Long, VendorConnection> getVendorConnection(@PathVariable("id") String vendorId) {
+//        Optional<Vendor> vendor = vendorRepository.findById(Long.valueOf(vendorId));
+//        if (vendor.isPresent()) {
+//            return vendorService.getVendorConnection(vendor.get());
+//        } else {
+//            return null;
+//        }
+        List<Long> vendorIdList = new ArrayList<>();
+        vendorIdList.add(2L);
+        vendorIdList.add(3L);
+        return getVendorConnections(vendorIdList);
+    }
+
+    Map<Long, VendorConnection> getVendorConnections(List<Long> vendorIdList) {
+        List<VendorConnection> vendorConnections = vendorConnectionRepository.findByVendorIdIn(vendorIdList);
+        Map<Long, VendorConnection> connectionMap = new HashMap<>();
+        vendorConnections.forEach(vendorConnection -> connectionMap.put(vendorConnection.getVendor().getId(), vendorConnection));
+        return connectionMap;
     }
 
 }
--- src/main/java/com/example/vendor/repository/VendorConnectionRepository.java
+++ src/main/java/com/example/vendor/repository/VendorConnectionRepository.java
@@ -5,8 +5,12 @@ import com.example.vendor.model.VendorConnection;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VendorConnectionRepository extends CrudRepository<VendorConnection, Long> {
 
     VendorConnection findByVendor(Vendor vendor);
+
+    List<VendorConnection> findByVendorIdIn(List<Long> vendorIdList);
 }
