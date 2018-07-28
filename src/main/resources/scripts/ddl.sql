7574b8489a4112e6d50e0a6a5dd46eff565970cd
mnabhiro <mnabhiro@qti.qualcomm.com>
2018-07-28 17:19:06 +0530

    Project files

---
+++ src/main/resources/scripts/ddl.sql
@@ -0,0 +1,7 @@
+create database vendor;
+insert into vendor values (1, 'vendor1', 'username1');
+insert into vendor values (2, 'vendor2', 'username2');
+insert into vendor values (3, 'vendor3', 'username3');
+insert into vendor_connection values (1, 'details1', 1);
+insert into vendor_connection values (2, 'details2', 2);
+insert into vendor_connection values (3, 'details3', 3);
